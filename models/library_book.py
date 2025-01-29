030dba136884b4d6b87059bb412218b3bdcfbc6a
DanielDS <danields020515@gmail.com>
2025-01-29 00:17:21 -0500

    Add library.book.is_loaned

--- models/library_book.py
+++ models/library_book.py
@@ -7,7 +7,18 @@ class Book(models.Model):
     isbn = fields.Char(required=True, string="ISBN", index=True)
     author = fields.Char()
     publication_date = fields.Date()
+    loan_ids = fields.One2many('library.loan', 'book_id')
+    is_loaned = fields.Boolean(compute='_compute_is_loaned', default=False)
     
     _sql_constraints = [
         ('check_isbn', 'UNIQUE(isbn)', 'The ISBN must be unique')
     ]
+    
+    @api.depends('loan_ids')
+    def _compute_is_loaned(self):
+        for book in self:
+            if 'pending' in book.loan_ids.mapped('status'):
+                book.is_loaned = True
+            else:
+                book.is_loaned = False
+            
